622d63a15f5de6e34c9ff3124fa8b0c9e63a3220
adsagot <blasterblade490@gmail.com>
2024-09-06 14:48:21 +0800

    Initial Commit for Activity 3

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,5 @@
 <?php
-
+use App\Models\Student;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +16,31 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/students/create', function () {
+    $student = new Student();
+    $student->first_name = 'Joren';
+    $student->last_name = 'Agot';
+    $student->email = 'blasterblade490@gmail.com';
+    $student->age = 22;
+    $student->save();
+    return view('Students Created!');
+});
+
+Route::get('/students/update', function () {
+    $student = Student::where('email','blasterblade490@gmail.com')->first();
+    $student->email = 'blasterblade490@gmail.com';
+    $student->age = 23;
+    $student->save();
+    return view('Students Updated!');
+});
+
+Route::get('/students/delete', function () {
+    $student = Student::where('email','blasterblade490@gmail.com')->first();
+    $student->delete();
+    return view('Students Delete!');
+});
+
+
+
+
